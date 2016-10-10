f250c9219798a4d878bf1db9b7c0a83f35401b75
Bent H. Pedersen <BHP@easv.dk>
2016-10-10 13:02:20 +0200

    Added BankAccount class

---
+++ TestBankProject/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBankProject
+{
+    public class BankAccount
+    {
+        public const double DEFAULT_INTEREST_RATE = 0.01;
+
+        public int AccountNumber { get; private set; }
+        public double Balance { get; private set; }
+        public double InterestRate { get; set; }
+
+        public BankAccount(int accNumber, double initialBalance, double initialInterestRate)
+        {
+            AccountNumber = accNumber;
+            Balance = initialBalance;
+            InterestRate = initialInterestRate;
+        }
+
+        public BankAccount(int accNumber, double initialBalance) : this(accNumber, initialBalance, DEFAULT_INTEREST_RATE) { }
+
+        public BankAccount(int accNumber) : this(accNumber, 0.0) { }
+    }
+}
