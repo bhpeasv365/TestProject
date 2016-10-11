378bc3c48736440fa6b24ce5ad4426914c61f6cd
Bent H. Pedersen <bhp@easv.dk>
2016-10-11 12:05:16 +0200

    Added features Deposit and Withdraw to BankAccount

--- TestBankProject/BankAccount.cs
+++ TestBankProject/BankAccount.cs
@@ -24,5 +24,21 @@ namespace TestBankProject
         public BankAccount(int accNumber, double initialBalance) : this(accNumber, initialBalance, DEFAULT_INTEREST_RATE) { }
 
         public BankAccount(int accNumber) : this(accNumber, 0.0) { }
+
+        public void deposit(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Unable to deposit a negative amount");
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < 0)
+                throw new ArgumentException("Unable to withdraw a negative amount");
+            if (amount > Balance)
+                throw new ArgumentException("Unable to withdraw an amount exceeding the balance");
+            Balance -= amount;
+        }
     }
 }
